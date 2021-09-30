0920795c049e42c78cb6ea8a2ee74ac3b3853608
MonChiSub <patrickjon@hotmail.co.uk>
2021-09-30 15:49:31 +0100

    Initial commit

---
+++ src/test/java/com/softwareinstitute/filmLibrary/FilmLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareinstitute.filmLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
